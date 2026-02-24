9f9ceeba0be78c8d1176c030be4fb5ed8b598a06
Vinutha Lakshmi <vinu7030@gmail.com>
2026-02-24 12:29:31 +0530

    Updated Quote

--- src/pages/Quote.jsx
+++ src/pages/Quote.jsx
@@ -1,25 +1,48 @@
+import { Link } from "react-router-dom";
+
+
 export default function Quote() {
   return (
     <section className="min-h-screen bg-black text-white flex flex-col items-center justify-center gap-8">
       
-      <h1 className="text-4xl font-bold">Request a Quote</h1>
+      <h1 className="text-4xl font-bold">Request a Quote...</h1>
 
       <div className="grid md:grid-cols-3 gap-6">
 
         <div className="glass p-8 rounded-2xl text-center">
-          <h2 className="text-xl font-semibold mb-3">Printing </h2>
-          <button className="btn bg-orange-500">Request</button>
-        </div>
+          <h2 className="text-xl font-semibold mb-3">🖨️📄PREMIUM PRINTZ & DESIGN🎨 </h2>
+          <Link
+    to="/getstarted"
+    className="btn bg-blue-500 inline-block px-6 py-2 rounded-full hover:scale-105 transition"
+  >
+    Request
+  </Link>
+</div>
+          
 
         <div className="glass p-8 rounded-2xl text-center">
-          <h2 className="text-xl font-semibold mb-3"> Events </h2>
-          <button className="btn bg-pink-500">Request</button>
-        </div>
+          <h2 className="text-xl font-semibold mb-3"> 🎉🎤Eventz🎊📅</h2>
+
+          <Link
+    to="/getstarted"
+    className="btn bg-pink-500 inline-block px-6 py-2 rounded-full hover:scale-105 transition"
+  >
+    Request
+  </Link>
+</div>
+          
 
         <div className="glass p-8 rounded-2xl text-center">
-          <h2 className="text-xl font-semibold mb-3">Digital Marketing </h2>
-          <button className="btn bg-purple-500">Request</button>
-        </div>
+          <h2 className="text-xl font-semibold mb-3"> 📱📢Digital Marketing📈🚀 </h2>
+
+          <Link
+    to="/getstarted"
+    className="btn bg-purple-500 inline-block px-6 py-2 rounded-full hover:scale-105 transition"
+  >
+    Request
+  </Link>
+</div>
+          
 
       </div>
     </section>
